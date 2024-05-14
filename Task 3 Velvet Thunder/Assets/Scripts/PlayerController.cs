0d100ab7dc4bd0477bec23717a7283fb58190965
jaw071 <jaw071@student.usc.edu.au>
2024-05-14 14:47:55 +1000

    created PlayerController script

---
+++ Task 3 Velvet Thunder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+// A player controller component will listen for inputs from the user
+// and communicate with the movement component whenever it reads any.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
