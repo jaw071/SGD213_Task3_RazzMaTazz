a004cf590c008f832db31721e290ebb5e9baf2f9
jaw071 <jaw071@student.usc.edu.au>
2024-05-14 15:44:17 +1000

    developed horizontal player movement
    across 2 scripts, InputManager for detecting keys and PlayerController for handling the movement

--- Task 3 Velvet Thunder/Assets/Scripts/MovementComponent.cs
+++ Task 3 Velvet Thunder/Assets/Scripts/MovementComponent.cs
@@ -8,10 +8,15 @@ using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
+    [SerializeField] private float horizontalPlayerAcceleration = 5000f;
+    // local references
+    private Rigidbody2D ourRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // populate ourRigidbody
+        ourRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,4 +24,29 @@ public class MovementComponent : MonoBehaviour
     {
         
     }
+
+    public void MovePlayer(float horizontalInput)
+    {
+        // a horizontalInput of 0 has no effect, as we want our ship to drift
+        if (horizontalInput != 0) {
+            //calculate our force to add
+            Vector2 forceToAdd = Vector2.right * horizontalInput * horizontalPlayerAcceleration * Time.deltaTime;
+            // apply forceToAdd to ourRigidbody
+            ourRigidbody.AddForce(forceToAdd);
+        }
+    }
+
+    public void MovePlayer(Vector2 direction)
+    {
+        // a horizontalInput of 0 has no effect, as we want our ship to drift
+        if (direction.magnitude != 0)
+        {
+            //calculate our force to add
+            Vector2 forceToAdd = direction * horizontalPlayerAcceleration * Time.deltaTime;
+            // apply forceToAdd to ourRigidbody
+            ourRigidbody.AddForce(forceToAdd);
+        }
+    }
 }
+
+
--- Task 3 Velvet Thunder/Assets/Scripts/PlayerController.cs
+++ Task 3 Velvet Thunder/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    // local references to other scripts
+    private MovementComponent movementComponent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementComponent = GetComponent<MovementComponent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // read the horizontal input axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // if movement input is not zero
+        if (horizontalInput != 0.0f)
+        {
+            // ensure the MovementComponent is populated to avoid errors
+            if (movementComponent != null)
+            {
+                // pass our movement input to our MovementComponent
+                movementComponent.MovePlayer(horizontalInput * Vector2.right);
+            }
+        }
     }
 }
