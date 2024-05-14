9bcab17d0dc23017cd4b7068471356cdc941b448
jaw071 <jaw071@student.usc.edu.au>
2024-05-14 14:49:35 +1000

    created MovementComponent script

---
+++ Task 3 Velvet Thunder/Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,22 @@
+// Movement for both the player and patrolling enemy game objects will
+// be provided by a movement component that interacts with the
+// rigidBody2D component of the objects.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
