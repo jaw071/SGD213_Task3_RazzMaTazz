e9d32db18d234d9af73dd3d7d6c150acad1f7328
James Pitor <pitorjames@gmail.com>
2024-05-22 12:14:11 +1000

    changed iBaseCollision.cs from abstract class to interface

--- Task 3 Velvet Thunder/Assets/Scripts/IBaseCollision.cs
+++ Task 3 Velvet Thunder/Assets/Scripts/IBaseCollision.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class IBaseCollision : MonoBehaviour
+
+interface IBaseCollision
 { 
     public abstract void OnTriggerEnter2D(Collider2D col);
 
