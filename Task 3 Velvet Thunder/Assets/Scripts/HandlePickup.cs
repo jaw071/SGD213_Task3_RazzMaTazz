92b68210078a7e25dd6337112f5622b3a62ea080
eel071 <e_l071@student.usc.edu.au>
2024-06-07 02:19:50 +1000

    Fixed colliders and added to Pickup script
    Put the wrong colliders on pickups.
    Set up the pickup script.

--- Task 3 Velvet Thunder/Assets/Scripts/HandlePickup.cs
+++ Task 3 Velvet Thunder/Assets/Scripts/HandlePickup.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandlePickup : MonoBehaviour
+public class HandlePickup : MonoBehaviour, IBaseCollision
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject jumpPickup;
+    public GameObject speedPickup;
+    
+    public void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.gameObject.tag == "Player")
+        {
+            GameObject player = col.gameObject;
+            HandleCollision(player);            
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.tag == "Player")
+        {
+            GameObject player = col.gameObject;
+            HandleCollision(player);
+        }
     }
+
+    public void HandleCollision(GameObject player)
+    {
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        MovementComponent movementComponent = player.GetComponent<MovementComponent>();
+
+        if (jumpPickup && !speedPickup)
+        {
+            //insert double jump coding
+            Debug.Log("Jump Pickup got");
+        }
+        if (speedPickup)
+        {
+            //insert double speed coding
+            Debug.Log("Speed Pickup got");
+        }
+              
+        Destroy(gameObject);
+    }
+    
+
 }
